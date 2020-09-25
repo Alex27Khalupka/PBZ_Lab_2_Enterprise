26945b8671198f42be384d5b787abc8580ad719b
Alex_Khalupka <alexkha2001@gmail.com>
2020-09-25 13:43:22 +0300

    POST methods and tests fixed

--- pkg/service/POSTservice.go
+++ pkg/service/POSTservice.go
@@ -3,15 +3,17 @@ package service
 import (
 	"database/sql"
 	"github.com/Alex27Khalupka/PBZ_Lab_2_Enterprise/pkg/model"
+	"log"
 	"time"
 )
 
 func POSTEmployee(db *sql.DB, employee model.Employee, divisionID string) error{
 
-	query := "INSERT INTO employees (employee_number, first_name, last_name, second_name, position, age, sex) VALUES " +
-		"(?, ?, ?, ?, ?, ?, ?)"
+	query := "INSERT INTO employees (employee_number, first_name, last_name, second_name, position, age, sex) " +
+		"VALUES ($1, $2, $3, $4, $5, $6, $7)"
 	stmt, err := db.Prepare(query)
 	if err != nil {
+		log.Println("kek ", query)
 		return err
 	}
 	defer stmt.Close()
@@ -36,9 +38,18 @@ func POSTEmployee(db *sql.DB, employee model.Employee, divisionID string) error{
 }
 
 func POSTMovementOfEmployees(db *sql.DB, employeeNumber string, divisionID string) error{
-	if _, err := db.Query("INSERT INTO movement_of_employees (employee_number, movement_date, division_number) VALUES" +
-	" (?, ?, ?)", employeeNumber, time.Now().Format("2020-01-01"), divisionID); err!=nil {
+	query := "INSERT INTO movement_of_employees (employee_number, movement_date, division_number) VALUES" +
+		" ($1, $2, $3)"
+	stmt, err := db.Prepare(query)
+	if err != nil {
 		return err
 	}
+	defer stmt.Close()
+	_, err = stmt.Exec(employeeNumber, time.Now().Format("2020-01-01"), divisionID)
+
+	if err !=nil{
+		return err
+	}
+
 	return nil
 }
--- pkg/service/POSTservice_test.go
+++ pkg/service/POSTservice_test.go
@@ -36,19 +36,12 @@ func TestService_POSTEmployee(t *testing.T) {
 		Sex:            "Female",
 	}
 
-	query1 := "INSERT INTO employees \\(employee_number, first_name, last_name, second_name, position, age, sex\\) " +
-		"VALUES \\(\\?, \\?, \\?, \\?, \\?, \\?, \\?\\)"
+	query := "INSERT INTO employees \\(employee_number, first_name, last_name, second_name, position, age, sex\\) " +
+		"VALUES \\(\\$1, \\$2, \\$3, \\$4, \\$5, \\$6, \\$7\\)"
 
-	query2 := "INSERT INTO movement_of_employees \\(employee_number, movement_date, division_number\\) VALUES" +
-	"\\(\\?, \\?, \\?\\)"
-
-	prep1 := mock.ExpectPrepare(query1)
-	prep1.ExpectExec().WithArgs(employee.EmployeeNumber, employee.FirstName, employee.LastName, employee.SecondName,
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs(employee.EmployeeNumber, employee.FirstName, employee.LastName, employee.SecondName,
 		employee.Position, employee.Age, employee.Sex).WillReturnResult(sqlmock.NewResult(0, 1))
-
-	prep2 := mock.ExpectPrepare(query2)
-	prep2.ExpectExec().WithArgs(employee.EmployeeNumber, time.Now().Format("2020-01-01"), "D5").
-		WillReturnResult(sqlmock.NewResult(0, 1))
 
 	err := POSTEmployee(db, employee, "D5")
 	assert.NoError(t, err)
@@ -72,19 +65,14 @@ func TestService_POSTEmployeeFail(t *testing.T) {
 		Sex:            "Female",
 	}
 
-	query1 := "INSERT INTO employees \\(employee_number, first_name, last_name, second_name, position, age, sex\\) " +
-		"VALUES \\(\\?, \\?, \\?, \\?, \\?, \\?, \\?\\)"
+	query := "INSERT INTO employees \\(employee_number, first_name, last_name, second_name, position, age, sex\\) " +
+		"VALUES \\(\\$1, \\$2, \\$3, \\$4, \\$5, \\$6, \\$7\\)"
 
-	query2 := "INSERT INTO movement_of_employees \\(employee_number, movement_date, division_number\\) VALUES" +
-		"\\(\\?, \\?, \\?\\)"
 
-	prep1 := mock.ExpectPrepare(query1)
-	prep1.ExpectExec().WithArgs(employee.EmployeeNumber, employee.FirstName, employee.LastName, employee.SecondName,
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs(employee.EmployeeNumber, employee.FirstName, employee.LastName, employee.SecondName,
 		employee.Position, "not int", employee.Sex).WillReturnResult(sqlmock.NewResult(0, 1))
 
-	prep2 := mock.ExpectPrepare(query2)
-	prep2.ExpectExec().WithArgs(employee.EmployeeNumber, time.Now().Format("2020-01-01"), "D5").
-		WillReturnResult(sqlmock.NewResult(0, 1))
 
 	err := POSTEmployee(db, employee, "D5")
 
@@ -92,3 +80,48 @@ func TestService_POSTEmployeeFail(t *testing.T) {
 
 }
 
+
+func TestService_POSTMovementOFEmployees(t *testing.T) {
+
+	db, mock := NewMock()
+
+	defer db.Close()
+
+	employee := model.Employee{
+		EmployeeNumber: "NUM",
+		FirstName:      "first_name",
+		LastName:       "last_name",
+		SecondName:     "second_name",
+		Position:       "position",
+		Age:            20,
+		Sex:            "Female",
+	}
+
+	query := "INSERT INTO movement_of_employees \\(employee_number, movement_date, division_number\\) VALUES " +
+	"\\(\\$1, \\$2, \\$3\\)"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs(employee.EmployeeNumber, time.Now().Format("2020-01-01"), "D5").
+	WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := POSTMovementOfEmployees(db, employee.EmployeeNumber, "D5")
+	assert.NoError(t, err)
+}
+
+func TestService_POSTMovementOFEmployeesFail(t *testing.T) {
+
+	db, mock := NewMock()
+
+	defer db.Close()
+
+
+	query := "INSERT INTO movement_of_employees \\(employee_number, movement_date, division_number\\) VALUES " +
+		"\\(\\$1, \\$2, \\$3\\)"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs("EMP", "kek", "D5").
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	err := POSTMovementOfEmployees(db, "kek", "D5")
+	assert.Error(t, err)
+}
